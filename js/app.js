a4b13662e33d54e10f5661584935c789c8a3e625
Sriya <sriya@Sriyas-MacBook-Pro.local>
2020-06-21 13:56:09 +0530

    Changes-21-june

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
-$(document).ready(function(){
-    let hamberger = document.querySelector('.hamberger');
-    let times = document.querySelector('.times');
-    let mobileNav = document.querySelector('.mobile-nav');
+let hamberger = document.querySelector('.container-hamberger');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
+let links= document.querySelector('.list');
 
+console.log('coming');
 
-    hamberger.addEventListener('click',function(){
+hamberger.addEventListener('click',function(){
+    console.log('coming');
         mobileNav.classList.add('open');
-    })
-
-    times.addEventListener('click',function(){
+})
+times.addEventListener('click',function(){
         mobileNav.classList.remove('open');
-    })
-
-});
+})
+links.addEventListener('click',function(){
+    mobileNav.classList.remove('open');
+})
